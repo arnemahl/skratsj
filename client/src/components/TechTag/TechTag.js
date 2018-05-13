023f52ced283c8613309218354acad71e5bf995d
Arne Mæhlum <arne.mahlum@gmail.com>
2018-05-13 20:15:27 +0200

    Restyle TechTag

--- client/src/components/TechTag/TechTag.js
+++ client/src/components/TechTag/TechTag.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 const TechTag = styled.span`
   display: inline-block;
 
-  margin: 5px;
-  padding: 5px 10px;
+  margin: 5px 4px;
+  padding: 3px 10px;
   line-height: 20px;
 
   font-size: 14px;
-  color: #333333;
-  border: solid 1px #d9d9d9;
+  color: white;
+  background-color: #b57e43;
   border-radius: 4px;
 `;
 
