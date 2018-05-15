cd92b56907964c05726a7b8ffa7b3bafb2989f3b
Arne Mæhlum <arne.mahlum@gmail.com>
2018-05-16 00:19:19 +0200

    Refactor: Move appropriate styling to Person.style.js
    Create extra wrapping element

--- client/src/pages/Person/Person.style.js
+++ client/src/pages/Person/Person.style.js
@@ -1,9 +1,24 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export const Page = styled.div`
+const Background = styled.div`
+  background-color: #1d1415;
+  --main-color: #2d6558;
+  --white: #e5e5e5;
+
+  padding: 33px 0;
+`;
+
+const Paper = styled.div`
   max-width: 800px;
-  margin: 33px auto;
+  margin: auto;
   padding: 25px;
   background-color: var(--white);
   color: #1e1e1e;
 `;
+
+export const Page = (props) => (
+  <Background>
+    <Paper {...props} />
+  </Background>
+);
