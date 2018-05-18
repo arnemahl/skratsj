7e6909ee97fbdffce3542fe11f7ebd2870f92f71
Arne Mæhlum <arne.mahlum@gmail.com>
2018-05-18 18:19:50 +0200

    Remove old project schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -2,7 +2,6 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import person from './person';
-import project from './project';
 import personalProject from './personalProject';
 import professionalProject from './professionalProject';
 import company from './company';
@@ -13,7 +12,6 @@ export default createSchema({
     name: 'default',
     types: schemaTypes.concat([
       person,
-      project,
       professionalProject,
       personalProject,
       company,
