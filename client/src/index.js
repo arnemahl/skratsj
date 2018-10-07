7c5b6b827b1ae4c8ee968c7c2274850d1beee0ee
Arne Mæhlum <arne.mahlum@gmail.com>
2018-10-07 13:28:47 +0200

    Import FontAwesome as npm dependency

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import './size.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
