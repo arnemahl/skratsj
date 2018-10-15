11d1f4e31310f0121037aa1fceb7ba908b4b423e
Arne Mæhlum <arne.mahlum@gmail.com>
2018-10-15 21:38:11 +0200

    Remove revoked token

--- client/src/sanity.js
+++ client/src/sanity.js
@@ -5,7 +5,6 @@ export const client = sanityClient({
   projectId: 'dyub8zvy',
   dataset: 'production',
   useCdn: true,
-  token: 'sk3UO9hKBDv9tcLf1mKuo5egd9l8ddH9EuiTTGujAOczry8Wzi8hRIhLe1OTQcRM414utAPKKMx3wk43JIMwD12O2LtUjDykMzXrxIkhEcWqWwJ00xEwNjBMo898uLUB0b1OY5yYw5dpZh9hQOOG50pqUDHyLE60tbGOKgpCViA9nGGr8kGB',
 });
 
 const builder = imageUrlBuilder(client);
